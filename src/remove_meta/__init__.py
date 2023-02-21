2a710b2558835d987a4f3856e611489c0aba01bb
Maxim Makovskiy <makovskiyms@gmail.com>
2023-02-21 13:08:24 +0600

    Bring back separate globs for each filetype

--- src/remove_meta/__init__.py
+++ src/remove_meta/__init__.py
@@ -7,17 +7,20 @@ def remove_meta():
     excel = win32.gencache.EnsureDispatch("Excel.Application")
     excel.Visible = False
     excel.DisplayAlerts = False
+    
+    filetypes = ["**/*.xls", "**/*.xlsx", "**/*.xlsm"]
 
-    for file in glob.iglob("**/*.xls?", recursive=True):
-        absolute_path = os.path.abspath(file)
-        print("Working with file:", absolute_path)
-        try:
-            wb = excel.Workbooks.Open(absolute_path)
-        except:
-            print("Error occurred when tried to open file:", absolute_path)
-            continue
-        wb.RemovePersonalInformation = True
-        wb.Save()
-        wb.Close()
+    for filetype in filetypes:
+        for file in glob.iglob(filetype, recursive=True):
+            absolute_path = os.path.abspath(file)
+            print("Working with file:", absolute_path)
+            try:
+                wb = excel.Workbooks.Open(absolute_path)
+            except:
+                print("Error occurred when tried to open file:", absolute_path)
+                continue
+            wb.RemovePersonalInformation = True
+            wb.Save()
+            wb.Close()
 
     excel.Quit()
